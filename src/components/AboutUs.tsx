38d310c5aa22b03004661eb39e9bafbd83401692
matias0621 <mattiascorrea@outlook.com>
2024-01-23 20:37:57 -0300

    responsive

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -4,21 +4,25 @@ import Carousel from "./Carousel"
 function AboutUs() {
     return (
         <section className="h-full flex flex-col font-primary">
-            <h2 className="text-5xl font-bold mx-4">Excelente servicio de comida</h2>
-            <p className="text-lg mt-7 mx-4">Hemos estado sirviendo comida de la mejor calidad, con nuestros mejores ingredientes, nuestro nombre garantiza sabor y calidad.</p>
-            <div className="bg-black p-4 mt-10">
-                <div className="mt-14 m-auto">
-                    <Image src="/images/hero-img.webp" alt="food" width={450} height={450}></Image>
-                </div>
-                <div className="text-white mt-14 pb-7">
-                    <h2 className="text-5xl font-bold mx-4">Preparadas con dedicación y listas para su entrega</h2>
-                    <p className="text-lg mt-7 mx-4">Los pedidos se tienen que pedir con 24hs de anticipación.</p>
-                    <button className="flex text-lg mt-7 mx-4 gap-1">Haz tu pedido <svg width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M5 12l14 0" />
-                        <path d="M13 18l6 -6" />
-                        <path d="M13 6l6 6" />
-                    </svg></button>
+            <div className="lg:flex lg:justify-between">
+                <h2 className="text-5xl font-bold mx-4 sm:text-center lg:px-4">Excelente servicio de comida</h2>
+                <p className="text-lg mt-7 mx-4 sm:text-center">Hemos estado sirviendo comida de la mejor calidad, con nuestros mejores ingredientes, nuestro nombre garantiza sabor y calidad.</p>
+            </div>
+            <div className="mx-16">
+                <div className="bg-black p-4 mt-10">
+                    <div className="mt-14 m-auto sm:mt-5">
+                        <Image src="/images/hero-img.webp" className="sm:w-full" alt="food" width={450} height={450}></Image>
+                    </div>
+                    <div className="text-white mt-14 pb-7">
+                        <h2 className="text-5xl font-bold mx-4 sm:">Preparadas con dedicación y listas para su entrega</h2>
+                        <p className="text-lg mt-7 mx-4">Los pedidos se tienen que pedir con 24hs de anticipación.</p>
+                        <button className="flex text-lg mt-7 mx-4 gap-1">Haz tu pedido <svg width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M5 12l14 0" />
+                            <path d="M13 18l6 -6" />
+                            <path d="M13 6l6 6" />
+                        </svg></button>
+                    </div>
                 </div>
             </div>
 
@@ -28,7 +32,11 @@ function AboutUs() {
                 <h2 className="text-4xl font-bold mx-4 text-center">¿Te preocupa que la vianda no sea tu agrado?</h2>
                 <p className="text-lg mt-10 mx-4">No te preocupes, nuestro servicio te ofrecerá una comida deliciosa y de calidad, con los gustos más tradicionales de la cocina.</p>
 
-                <Image className="mt-14 m-auto" src="/images/hero-img.webp" width={450} height={450} alt="food"></Image>
+                <div className="lg:flex lg:w-full">
+                    <Image className="mt-14 m-auto sm:mt-10 sm:w-full sm:px-4 lg:w-96 lg:h-96 lg:m-0 lg:px-0 lg:mt-8 lg:mr-4" src="/images/hero-img.webp" width={450} height={450} alt="food"></Image>
+                    <Image className="hidden mt-14 m-auto sm:mt-10 sm:w-full sm:px-4 lg:block lg:w-96 lg:h-96 lg:m-0 lg:px-0 lg:mt-8 lg:mr-4" src="/images/hero-img.webp" width={450} height={450} alt="food"></Image>
+                    <Image className="hidden mt-14 m-auto sm:mt-10 sm:w-full sm:px-4 lg:w-96 lg:block lg:h-96 lg:m-0 lg:px-0 lg:mt-8" src="/images/hero-img.webp" width={450} height={450} alt="food"></Image>
+                </div>
 
                 <h2 className="text-4xl font-bold mt-16 mx-4 text-center">¿Tienes más preguntas sobre nuestro servicio?</h2>
                 <p className="text-xl mt-10 mx-4 text-center">Aquí abajo está la respuesta a tu pregunta.</p>
@@ -45,4 +53,4 @@ function AboutUs() {
     )
 }
 
-export default AboutUs
+export default AboutUs
